fa9be5b971cb97421b7dfd6a6770d7dfc9d053f4
Leandro <leandroleance.26@gmail.com>
2024-05-10 10:06:07 -0300

    Finalizado Calculadora de IMC

--- JavaScript/14_Calculadora-IMC/js/script.js
+++ JavaScript/14_Calculadora-IMC/js/script.js
@@ -39,10 +39,15 @@ const data = [
 
 //todo: Seleção dos elemtos
 const imcTable = document.querySelector("#imc-table");
-const height = document.querySelector("#height");
+const heightInput = document.querySelector("#height");
 const weightInput = document.querySelector("#weight");
 const calcBtn = document.querySelector("#calc-btn");
 const clearBtn = document.querySelector("#clear-btn");
+const imcNumber = document.querySelector("#imc-number span");
+const imcInfo = document.querySelector("#imc-info span");
+const backBtn = document.querySelector("#back-btn");
+const calcContainer = document.querySelector("#calc-container");
+const resultContainer = document.querySelector("#result-container");
 
 //todo: Funções
 function createTable(data) {
@@ -67,5 +72,94 @@ function createTable(data) {
   });
 }
 
+function cleanInput() {
+  heightInput.value = "";
+  weightInput.value = "";
+  imcNumber.classList = "";
+  imcInfo.classList = "";
+}
+
+function validDigits(text) {
+  return text.replace(/[^0-9,]/g, "");
+}
+
+function calcImc(weight, height) {
+  const imc = (weight / (height * height)).toFixed(1);
+  return imc;
+}
+
+function showOrHideResults() {
+  calcContainer.classList.toggle("hide");
+  resultContainer.classList.toggle("hide");
+}
+
 //todo: Inicialização
 createTable(data);
+
+//todo: EVENTOS
+
+[heightInput, weightInput].forEach((el) => {
+  el.addEventListener("input", (e) => {
+    const updateValue = validDigits(e.target.value);
+    e.target.value = updateValue;
+  });
+});
+
+calcBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const weight = +weightInput.value.replace(",", "."); //! Com o .replace eu troco de lugar caracteres, neste caso a virgula por ponto, já que o JS reconhece apenas pontos para fazer cálculos
+  const height = +heightInput.value.replace(",", "."); //! o sinal de + faz a conversão de strings obtidas nos inputs por números float, para que o JS faça os cálculos
+
+  if (!weight || !height) return;
+
+  const imc = calcImc(weight, height);
+
+  let info;
+
+  data.forEach((item) => {
+    if (imc >= item.min && imc <= item.max) {
+      info = item.info;
+    }
+  });
+
+  if (!info) return;
+
+  imcNumber.innerText = imc;
+  imcInfo.innerText = info;
+
+  switch (info) {
+    case "Magreza":
+      imcNumber.classList.add("low");
+      imcInfo.classList.add("low");
+      break;
+    case "Normal":
+      imcNumber.classList.add("good");
+      imcInfo.classList.add("good");
+      break;
+    case "Sobrepeso":
+      imcNumber.classList.add("medium");
+      imcInfo.classList.add("medium");
+      break;
+    case "Obesidade":
+      imcNumber.classList.add("high");
+      imcInfo.classList.add("high");
+      break;
+    case "Obesidade grave":
+      imcNumber.classList.add("very-high");
+      imcInfo.classList.add("very-high");
+      break;
+  }
+
+  showOrHideResults();
+});
+
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  cleanInput();
+});
+
+backBtn.addEventListener("click", () => {
+  cleanInput();
+  showOrHideResults();
+});
